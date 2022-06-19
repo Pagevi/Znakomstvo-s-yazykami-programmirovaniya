8db3c927e1195d24522bb1b093f57a57afd3a2f4
Gennadiy <pgv.beq@gmail.com>
2022-06-19 21:25:58 +0300

    add Lesson03 v.1

---
+++ Lesson03/Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int Promt (string message)
+{
+    Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int x1 = Promt("Введите координаты x первой точки: ");
+int y1 = Promt("Введите координаты y первой точки: ");
+int z1 = Promt("Введите координаты z первой точки: ");
+
+int x2 = Promt("Введите координаты x второй точки: ");
+int y2 = Promt("Введите координаты y второй точки: ");
+int z2 = Promt("Введите координаты z второй точки: ");
+
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+Console.WriteLine($"Расстояние между точками: {result:F2}");
---
+++ Lesson03/Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Promt (string message)
+{
+    Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+void Result(int N)
+{
+    int index = 1;
+    while (index <= N)
+    {
+        Console.WriteLine(index * index * index);
+        index++;
+    }
+}
+
+int number = Promt("Введите число N: ");
+
+Result(number);
