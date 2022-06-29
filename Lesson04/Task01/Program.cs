2e59c2d0ddb9bc3cb8c34868ae17d84adda7555a
Gennadiy <pgv.beq@gmail.com>
2022-06-29 21:16:54 +0300

    add Lesson04 v1.0

---
+++ Lesson04/Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int PromptNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int PowerNat(int based, int power)
+{
+    if (power == 0) return 1;
+    int result = based;
+    for (int i = 1; i < power; i++)
+    {
+        result = result * based;
+    }
+    return result;
+}
+
+int A = PromptNumber("Введите число А ");
+int B = PromptNumber("Введите число В ");
+Console.WriteLine($"{A} в натуральной степени {B} составляет {PowerNat(A, B)}");
---
+++ Lesson04/Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int PromptNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SumNum(int number)
+{
+    int num = 0;
+    while (number > 0)
+    {
+        num = num + number % 10;
+        number = number / 10;
+    }
+    return num;
+}
+
+int number = PromptNumber("Введите число ");
+Console.WriteLine($"Сумма цифр в числе {number} составляет {SumNum(number)}");
