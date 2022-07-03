273ca8756ee589df648c38d8984bab18ebbfc335
Gennadiy <pgv.beq@gmail.com>
2022-07-03 20:25:21 +0300

    add Lesson06 v.1.0

---
+++ Lesson06/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] Numbers(int M)
+{
+    int[] numM = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        numM[i] = Prompt($"Введите {i + 1} из {M} чисел ");
+    }
+    return numM;
+}
+int PositiveNumbers(int[] array)
+{
+    int counter = 0;
+    foreach (int element in array)
+    {
+        if (element > 0) counter++;
+    }
+    return counter;
+}
+
+
+int M = Prompt("Сколько чисел необходимо ввести? ");
+int[] myNumbers = Numbers(M);
+Console.WriteLine($"Количество положительных чисел из них: {PositiveNumbers(myNumbers)}");
---
+++ Lesson06/Task02/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double number = double.Parse(Console.ReadLine());
+    return number;
+}
+
+double[] FindIntersection(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        double[] except = new double[1];
+        if (b1 == b2)
+        {
+            except[0] = 1;
+            return except;
+        }
+        else
+        {
+            return except;
+        }
+    }
+    double[] answer = new double[2];
+    answer[0] = (b2 - b1) / (k1 - k2);
+    answer[1] = k1 * answer[0] + b1;
+    return answer;
+}
+void Answer(double[] result, double k1, double b1, double k2, double b2)
+{
+    if (result.Length == 1)
+    {
+        if (result[0] == 0) Console.WriteLine("Прямые не пересекаются");
+        else { Console.WriteLine("Прямые одинаковы"); }
+    }
+    else
+    {
+        Console.WriteLine($"Точка пересечения линий y={k1}x+{b1} и y={k2}x+{b2}: ({result[0]:f2}, {result[1]:f2})");
+    }
+}
+
+double k1 = Prompt("Введите значение k1 в y=k1*x+b1 для первой линии: ");
+double b1 = Prompt("Введите значение b1 в y=k1*x+b1 для первой линии: ");
+double k2 = Prompt("Введите значение k2 в y=k2*x+b2 для второй линии: ");
+double b2 = Prompt("Введите значение b2 в y=k2*x+b2 для второй линии: ");
+Answer(FindIntersection(k1, b1, k2, b2), k1, b1, k2, b2);
