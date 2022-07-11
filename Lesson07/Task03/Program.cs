7aa51b45f357799ae422d511a2a68958e634b23d
Gennadiy <pgv.beq@gmail.com>
2022-07-11 20:00:34 +0300

    add Lesson07 v1.0

---
+++ Lesson07/Task03/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// Задача 3: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string intString = Console.ReadLine();
+    return int.Parse(intString);
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}" + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintAnswer(double[] list)
+{
+    Console.WriteLine("Среднее арифметическое каждого столбца: ");
+    foreach (double element in list)
+    {
+        Console.Write($"{element:f2}" + "\t");
+
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateRandomMatrix(int row, int column, int min, int max)
+{
+    var rnd = new Random();
+    int[,] newMatrix = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            newMatrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return newMatrix;
+}
+
+double[] FindAverageColumns(int[,] matrix)
+{
+    double runningSum = 0;
+    double[] columns = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        runningSum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            runningSum = runningSum + matrix[i, j];
+        }
+        columns[j] = runningSum / matrix.GetLength(0);
+    }
+    return columns;
+}
+
+
+
+int n = Prompt("Введите число строк ");
+int m = Prompt("Введите число столбцов ");
+int min = Prompt("Введите минимум ");
+int max = Prompt("Введите максимум ");
+
+
+
+int[,] myArray = CreateRandomMatrix(n, m, min, max);
+
+PrintArray(myArray);
+
+PrintAnswer(FindAverageColumns(myArray));
