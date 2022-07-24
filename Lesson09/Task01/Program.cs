7ed2b39e970bf14fbf064020c86a984d6287f684
Gennadiy <pgv.beq@gmail.com>
2022-07-24 21:07:22 +0300

    add Lesson09 v1.0

---
+++ Lesson09/Task01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void RecursiveMToN(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    else
+    {
+        {
+            if (M % 2 == 0)
+            {
+                System.Console.Write($"{M}, ");
+            }
+            RecursiveMToN(M + 1, N);
+        }
+    }
+}
+
+int Prompt(string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Введите нижний предел");
+    int n = Prompt("Введите верхний предел");
+    RecursiveMToN(m, n);
+
+}
+
+
+
+Solve();
---
+++ Lesson09/Task02/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int RecursiveMToNSum(int M, int N)
+{
+    int total = 0;
+    if (N < M)
+    {
+        return 0;
+    }
+    else
+    {
+        {
+            total = M + RecursiveMToNSum(M + 1, N);
+            return total;
+
+        }
+    }
+
+}
+
+int Prompt(string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Введите нижний предел");
+    int n = Prompt("Введите верхний предел");
+    System.Console.WriteLine(RecursiveMToNSum(m, n));
+
+}
+
+
+
+Solve();
